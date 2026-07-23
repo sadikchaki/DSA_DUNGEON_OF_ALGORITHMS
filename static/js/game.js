97544446161576101a70f676bc6fd2b736864f30
sadikchaki <sadikchaki0208@gmail.com>
2026-07-23 20:21:13 +0530

    0.0

--- static/js/game.js
+++ static/js/game.js
@@ -7,6 +7,16 @@
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+function resizeCanvas() {
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+}
+
+window.addEventListener("resize", resizeCanvas);
+
+resizeCanvas();
 
 // Player Object
 
